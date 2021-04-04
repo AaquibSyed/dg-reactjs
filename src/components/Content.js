2f3caceca4cad2e26923caa8f461f10c936f8ba7
AaquibSyed <m.nsyedaaquib@gmail.com>
2021-04-04 19:44:05 +0530

    content-component done

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ContentItem from "./ContentItem";
+import uuid from "react-uuid";
 import "./Content.css";
+
 function Content() {
-  return <div className="content">i am the content</div>;
+  const [data, setdata] = useState({});
+  useEffect(() => {
+    axios
+      .get("./data/CONTENTLISTINGPAGE-PAGE1.json")
+      .then((res) => res.data)
+      .then((data) => {
+        console.log(data.page["content-items"].content);
+        setdata(data.page["content-items"]);
+      });
+    console.log(data);
+  }, []);
+
+  return (
+    <div className="content">
+      {data?.content?.map((item) => (
+        <ContentItem
+          title={item.name}
+          key={uuid()}
+          poster={item["poster-image"]}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default Content;
