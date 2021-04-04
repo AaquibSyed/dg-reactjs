82ec135cc88369180886f7268879ab4b46aabdd6
AaquibSyed <m.nsyedaaquib@gmail.com>
2021-04-04 12:23:46 +0530

    Header Layout done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,24 @@
 import React from "react";
 import "./Header.css";
-
+import SearchIcon from "@material-ui/icons/Search";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 function Header() {
   return (
     <div className="header">
-      <div className="header__back">back button</div>
-      <div className="header__search">search</div>
-      <div className="header__seachIcon">icon</div>
+      <div className="header__back">
+        <ArrowBackIcon />
+      </div>
+      <div className="header__search">
+        <input
+          type="text"
+          maxLength="30"
+          className="search__input"
+          placeholder="search..."
+        />
+      </div>
+      <div className="header__searchIcon">
+        <SearchIcon />
+      </div>
     </div>
   );
 }
